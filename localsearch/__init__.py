5ef982884aa39001130c7b01653e48a6102ba3e8
Thomas Kloiber <thomas.kloiber@leftshift.one>
2023-08-08 16:53:40 +0200

    Import search config

--- localsearch/__init__.py
+++ localsearch/__init__.py
@@ -1,2 +1,2 @@
 from .__spi__.model import RankedDocument, ScoredDocument, IndexedDocument, Document
-from .pipeline import SearchPipeline, IndexPipeline
+from .pipeline import SearchPipeline, IndexPipeline, SearchConfig
