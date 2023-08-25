0e45cab5930bdb5856ba74b8bc613d74a86e9ead
Thomas Kloiber <thomas.kloiber@leftshift.one>
2023-08-25 14:27:31 +0200

    Add character level text splitter

--- localsearch/__util__/string_utils.py
+++ localsearch/__util__/string_utils.py
@@ -29,3 +29,26 @@ def lemmatize(text: str, lang: Lang):
 def md5(text: str):
     from hashlib import md5
     return str(md5(text.encode("utf-8")).hexdigest())
+
+
+# TODO: rename chunk_size, window_size to window_size, overlap
+def split_sentences(
+        text: str,
+        language: Lang,
+        chunk_size: int = 3,
+        window_size: int = 1
+) -> List[str]:
+    import pysbd
+    seg = pysbd.Segmenter(language=language, clean=False)
+    sentences = seg.segment(text)
+
+    return [" ".join(sentences[i:i + chunk_size]) for i in range(0, len(sentences), chunk_size-window_size)]
+
+
+def split_characters(
+        text: str,
+        window_size: int = 500,
+        overlap: int = 100
+) -> List[str]:
+
+    return [text[i: i+window_size] for i in range(0, len(text), window_size-overlap)]
