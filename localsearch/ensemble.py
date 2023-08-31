2e97d7e6ecabfacb06b15c4770d2e9a3e0d297ce
c7nw3r <christian.weber@leftshift.one>
2023-08-31 15:20:14 +0200

    bugfix compilation error SearchEnsemble

--- localsearch/ensemble.py
+++ localsearch/ensemble.py
@@ -19,5 +19,8 @@ class SearchEnsemble(Searcher):
     def remove(self, idx: str):
         [e.remove(idx) for e in self.searchers]
 
-    def search_by_source(self, source: str) -> List[Document]:
+    def search_by_source(self, source: str, n: Optional[int] = None) -> List[Document]:
         return flatten([e.search_by_source(source) for e in self.searchers])
+
+    def remove_by_source(self, source: str):
+        [e.remove_by_source(source) for e in self.searchers]
